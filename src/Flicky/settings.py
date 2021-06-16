fa925fb0eed53181e93d89a92c45a6c283048e1c
Artem <5hv5art@gmail.com>
2021-06-16 18:24:09 +0300

    setting dubug to False

--- src/Flicky/settings.py
+++ src/Flicky/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["flicky-films.herokuapp.com",
                 "stormy-wave-94553.herokuapp.com",
